6e9a04c2db4602b2eae6b1396904c5b4d634017e
Kateryna Bugaieva <sonnensmile@gmail.com>
2020-07-23 19:08:51 +0200

    display obj

--- src/App.js
+++ src/App.js
@@ -3,28 +3,27 @@ import "./App.css";
 import * as styles from "./styles";
 
 class App extends Component {
-  name = "React";
+  name = "Display jsx objects";
   render() {
-   
-
-    const isHere = "is here";
+    const var1 = {
+      key1: "some data"
+    }
     return (
       <div className="App">
         <div style={styles.styles}>
           {this.name}
-          <button>Button</button>
         </div>
+        {React.createElement("button", { className: "App" }, "Show me the obj")}
+        &nbsp;
 
-        {React.createElement('button', {className:"App"}, "React")}
-        &nbsp; 
+        {React.createElement("div", { className: "app" }, var1.key1)}
+
       </div>
     );
   }
 }
 
 export default App;
-
-
 
 // nice tool to see the code online
 // https://babeljs.io/
