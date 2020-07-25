1e6e903316efaa1c62441a86920d2ebde2e00156
Kateryna Bugaieva <sonnensmile@gmail.com>
2020-07-25 19:14:48 +0200

    component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React, { Component } from "react";
 import "./App.css";
+import Component1 from "./functional/component1";
 import * as styles from "./styles";
 
 class App extends Component {
   name = "Display jsx objects";
   render() {
     const var1 = {
-      key1: "some data"
-    }
+      key1: "some data",
+    };
     return (
       <div className="App">
-        <div style={styles.styles}>
-          {this.name}
-        </div>
+        <div style={styles.styles}>{this.name}</div>
         {React.createElement("button", { className: "App" }, "Show me the obj")}
         &nbsp;
-
         {React.createElement("div", { className: "app" }, var1.key1)}
-
+       
+        <Component1 />
       </div>
     );
   }
