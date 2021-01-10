cab1897f6c4a1d5a56c167b732ad1d20f630a9e2
Kateryna Bugaieva <sonnensmile@gmail.com>
2021-01-10 17:45:32 +0100

    jsx styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
+import "./App.css";
+import * as styles from "./styles";
 
 class App extends Component {
-  name = "React"
+  name = "React";
   render() {
-    const isHere = "is here"
-    return <div className="App">{this.name} &nbsp; {isHere}
-    </div>;
+   
+
+    const isHere = "is here";
+    return (
+      <div className="App">
+        <div style={styles.styles}>
+          {this.name} &nbsp;
+          <button>Button</button>
+        </div>
+        {isHere}
+      </div>
+    );
   }
 }
 
